24eaf6d29fc3a764c2d5a4e41600e509c70a3d7a
GabrielMBoeira <gboeira777@gmail.com>
2020-04-01 02:22:56 -0300

    adicionando arquivos

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="src/assets/css/template/index.css">
+    <link rel="stylesheet" href="src/assets/css/icofont.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>CallSystem</title>
+</head>
+<body>
+   <header class="header">
+      <img src="src/assets/images/phone.png" class="mt-3" alt="phone" width="auto" height="80">
+      <div class="title-group">
+         <h1 class="title">Call System</h1>
+         <h2 class="subtitle">Atendimento de chamados</h2>
+      </div>
+      <div class="dropdown">
+            <div class="dropdown-button">
+                <span>Usuário: </span>
+                <span>Gabriel</span>
+                <i class="icofont-simple-down"></i>
+            </div>
+            <div class="dropdown-content mt-2">
+                <ul class="nav-list">
+                    <li class="nav-item"> 
+                        <a href="#">
+                            <i class="icofont-logout mr-2"></i>
+                            Sair
+                        </a>  
+                    </li>
+                </ul>
+            </div>
+        </div>
+   </header>
+   <aside class="aside"></aside>
+   <main class="main"></main>
+   <footer class="footer"></footer>
+</body>
+</html>
---
+++ src/index.php
@@ -0,0 +1,43 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="src/assets/css/style.css">
+    <link rel="stylesheet" href="src/assets/css/icofont.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>CallSystem</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <div class="header-img">
+                <img src="src/assets/images/phone.png" alt="phone">
+            </div>
+            <div class="title">
+                <h1 class="mt-2">CallSystem</h1>
+                <h2>Controle de chamados</h2>
+            </div>
+            <div class="header-user">
+                <span>Usuário:</span>
+                <span>Gabriel</span>
+                <i class="icofont-travelling ml-2"></i>
+            </div>
+        </div>
+    </header>
+    <aside class="aside">
+        <div class="menu">
+            
+        </div>
+    </aside>
+    <main>
+        
+    </main>
+    <footer>
+        
+    </footer>
+</body>
+</html>
