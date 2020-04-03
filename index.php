c0effabfc3f0193cd7f6fbcc22920a02156ceef8
GabrielMBoeira <gboeira777@gmail.com>
2020-04-02 22:13:12 -0300

    adicionando arquivos

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="src/assets/css/template/index.css">
@@ -11,33 +12,56 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>CallSystem</title>
 </head>
+
 <body>
-   <header class="header">
-      <img src="src/assets/images/phone.png" class="mt-3" alt="phone" width="auto" height="80">
-      <div class="title-group">
-         <h1 class="title">Call System</h1>
-         <h2 class="subtitle">Atendimento de chamados</h2>
-      </div>
-      <div class="dropdown">
-            <div class="dropdown-button">
-                <span>Usuário: </span>
-                <span>Gabriel</span>
-                <i class="icofont-simple-down"></i>
-            </div>
-            <div class="dropdown-content mt-2">
-                <ul class="nav-list">
-                    <li class="nav-item"> 
-                        <a href="#">
-                            <i class="icofont-logout mr-2"></i>
-                            Sair
-                        </a>  
+    <header class="header">
+        <img src="src/assets/images/phone.png" class="phone" alt="">
+        <div class="title-group">
+            <h1 class="title"></h1>
+            <h2 class="subtitle"></h2>
+        </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                     </li>
                 </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
             </div>
-        </div>
-   </header>
-   <aside class="aside"></aside>
-   <main class="main"></main>
-   <footer class="footer"></footer>
+        </nav>
+    </header>
+    <aside class="aside"></aside>
+    <main class="main"></main>
+    <footer class="footer"></footer>
 </body>
+
 </html>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
