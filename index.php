baf03617c43e6424310ae58d9dfc913c05468161
GabrielMBoeira <gboeira777@gmail.com>
2020-04-08 00:12:41 -0300

    adicionando arquivos

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="src/assets/css/template/index.css">
+    <link rel="stylesheet" href="src/assets/css/template/main.css">
     <link rel="stylesheet" href="src/assets/css/icofont.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>CallSystem</title>
@@ -61,87 +62,14 @@
         </button>
     </aside>
 
-    <main class="main">
-        <div class="content">
-            <div class="content-title">
-                <i class="icofont-network mr-3 my-5"></i>
-                <div>
-                    <h1>Chamados em aberto</h1>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-body">
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th>Chamado</th>
-                                <th>Placa</th>
-                                <th>Status</th>
-                                <th>Atuante</th>
-                                <th>Tempo</th>
-                                <th>Editar</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th>123</th>
-                                <td>QHG4147</td>
-                                <td>aberto</td>
-                                <td>comercial</td>
-                                <td>00:00:00</td>
-                                <td><a href="" class="btn rounded-circle"><i class="icofont-edit-alt"></i></a></td>
-                            </tr>
-                            <tr>
-                                <th>123</th>
-                                <td>QHG4147</td>
-                                <td>aberto</td>
-                                <td>comercial</td>
-                                <td>00:00:00</td>
-                                <td><a href="" class="btn rounded-circle"><i class="icofont-edit-alt"></i></a></td>
-                            </tr>
-                            <tr>
-                                <th>123</th>
-                                <td>QHG4147</td>
-                                <td>aberto</td>
-                                <td>comercial</td>
-                                <td>00:00:00</td>
-                                <td><a href="" class="btn rounded-circle"><i class="icofont-edit-alt"></i></a></td>
-                            </tr>
-                            <tr>
-                                <th>123</th>
-                                <td>QHG4147</td>
-                                <td>aberto</td>
-                                <td>comercial</td>
-                                <td>00:00:00</td>
-                                <td><a href="" class="btn rounded-circle"><i class="icofont-edit-alt"></i></a></td>
-                            </tr>
-                            <tr>
-                                <th>123</th>
-                                <td>QHG4147</td>
-                                <td>aberto</td>
-                                <td>comercial</td>
-                                <td>00:00:00</td>
-                                <td><a href="" class="btn rounded-circle"><i class="icofont-edit-alt"></i></a></td>
-                            </tr>
-                            <tr>
-                                <th>123</th>
-                                <td>QHG4147</td>
-                                <td>aberto</td>
-                                <td>comercial</td>
-                                <td>00:00:00</td>
-                                <td><a href="" class="btn rounded-circle"><i class="icofont-edit-alt"></i></a></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="card-footer">
+    <?php require_once('src/template/main.php') ?>
+    
 
-                </div>
-            </div>
+    <footer class="footer">
+        <div class="footer-assign">
+            Desenvolvido por Gabriel Boeira
         </div>
-    </main>
-
-    <footer class="footer"></footer>
+    </footer>
 </body>
 
 </html>
---
+++ src/template/main.php
@@ -0,0 +1,81 @@
+
+
+<main class="main">
+<div class="content">
+    <div class="content-title">
+        <i class="icofont-network mr-3 my-5"></i>
+        <div>
+            <h1>Chamados em aberto</h1>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Chamado</th>
+                        <th>Placa</th>
+                        <th>Status</th>
+                        <th>Atuante</th>
+                        <th>Tempo</th>
+                        <th>Editar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>123</th>
+                        <td>QHG4147</td>
+                        <td>aberto</td>
+                        <td>comercial</td>
+                        <td>00:00:00</td>
+                        <td><a href="" class="btn rounded-circle"><i class="icofont-edit-alt"></i></a></td>
+                    </tr>
+                    <tr>
+                        <th>123</th>
+                        <td>QHG4147</td>
+                        <td>aberto</td>
+                        <td>comercial</td>
+                        <td>00:00:00</td>
+                        <td><a href="" class="btn rounded-circle"><i class="icofont-edit-alt"></i></a></td>
+                    </tr>
+                    <tr>
+                        <th>123</th>
+                        <td>QHG4147</td>
+                        <td>aberto</td>
+                        <td>comercial</td>
+                        <td>00:00:00</td>
+                        <td><a href="" class="btn rounded-circle"><i class="icofont-edit-alt"></i></a></td>
+                    </tr>
+                    <tr>
+                        <th>123</th>
+                        <td>QHG4147</td>
+                        <td>aberto</td>
+                        <td>comercial</td>
+                        <td>00:00:00</td>
+                        <td><a href="" class="btn rounded-circle"><i class="icofont-edit-alt"></i></a></td>
+                    </tr>
+                    <tr>
+                        <th>123</th>
+                        <td>QHG4147</td>
+                        <td>aberto</td>
+                        <td>comercial</td>
+                        <td>00:00:00</td>
+                        <td><a href="" class="btn rounded-circle"><i class="icofont-edit-alt"></i></a></td>
+                    </tr>
+                    <tr>
+                        <th>123</th>
+                        <td>QHG4147</td>
+                        <td>aberto</td>
+                        <td>comercial</td>
+                        <td>00:00:00</td>
+                        <td><a href="" class="btn rounded-circle"><i class="icofont-edit-alt"></i></a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="card-footer">
+
+        </div>
+    </div>
+</div>
+</main>
