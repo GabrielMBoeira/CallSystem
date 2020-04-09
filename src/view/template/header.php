2f0bf1937d67d8ce8d17badd92131d04a43b1030
GabrielMBoeira <gboeira777@gmail.com>
2020-04-08 23:19:51 -0300

    adicionando arquivos

---
+++ src/view/template/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="src/assets/css/template/index.css">
+    <link rel="stylesheet" href="src/assets/css/template/main.css">
+    <link rel="stylesheet" href="src/assets/css/icofont.min.css">
+
+    <!-- <link rel="stylesheet" href="assets/css/comum.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/icofont.min.css">
+    <link rel="stylesheet" href="assets/css/login.css"> -->
+
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>CallSystem</title>
+</head>
+
+<body>
+    <header class="header">
+        <img src="src/assets/images/phone.png" class="phone" width="auto" height="100" alt="">
+        <div class="title-group">
+            <h1 class="title">Call System</h1>
+            <h2 class="subtitle">Atendimento de chamados</h2>
+        </div>
+        <div class="dropdown mr-5">
+            <div class="dropdown-button">
+                <span>Login: Gabriel Boeira</span>
+                <i class="icofont-simple-down"></i>
+            </div>
+            <div class="dropdown-content">
+                <ul class="nav-list">
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Configurações</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Sair</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
