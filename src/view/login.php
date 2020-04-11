6487ff2ff382f0d44234a761c028cd18e46d7cc8
GabrielMBoeira <gboeira777@gmail.com>
2020-04-10 23:24:47 -0300

    adicionando arquivos

--- src/view/login.php
+++ src/view/login.php
@@ -13,13 +13,26 @@
 <body>
     <form action="#" method="post">
         <div class="login-card card">
-            <div class="card-header d-flex justify-content-center">
+            <div class="card-header d-flex justify-content-flexstart">
+                <img src="src/assets/images/phone.png" alt="phone" width="auto" height="32">
                 Login
             </div>
             <div class="card-body">
                 <div class="form-group">
                     <label for="email">E-mail</label>
                     <input type="email" id="email" name="email" placeholder="Digite o seu e-mail" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password">Senha</label>
+                    <input type="password" id="password" name="password" placeholder="Digite sua senha" class="form-control"><br>
+                    <div class="group-confirm">
+                        <a href="" class="forgot">Esqueci a senha</a>
+                        <button class=" btn btn-primary help">Entrar</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer">
+
             </div>
         </div>
     </form>
