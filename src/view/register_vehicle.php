e8bf0561000902876251645fb12dea2f00bf3289
GabrielMBoeira <gboeira777@gmail.com>
2020-04-13 21:45:58 -0300

    adicionando arquivos

--- src/view/register_vehicle.php
+++ src/view/register_vehicle.php
@@ -12,39 +12,40 @@ if (count($_POST) > 0) {
     $dados = $_POST;
     $erros = [];
 
-    if (trim($dados['placa']) === "") {
-        $erros['placa'] = 'placa é obrigatório';
+        if (trim($dados['placa']) === "") {
+            $erros['placa'] = 'placa é obrigatório';
+        }
+
+        if (trim($dados['nome']) === "") {
+            $erros['nome'] = 'nome é obrigatório';
+        }
+
+        if (trim($dados['telefone']) === "") {
+            $erros['telefone'] = 'telefone é obrigatório';
+        }
+
+
+    if (!count($erros)) {
+        require_once "src/db/conexao.php";
+
+        $sql = "INSERT INTO veiculo (placa, motorista, telefone) VALUES (?, ?, ?)";
+
+        $conexao = novaConexao();
+        $stmt = $conexao->prepare($sql);
+
+        $params = [
+            $dados['placa'],
+            $dados['motorista'],
+            $dados['telefone'],
+        ];
+
+        $stmt->bind_param("sss", ...$params);
+
+        if ($stmt->execute()) {
+            unset($dados); //Depois de inserir limpar os dados
+        }
+        $conexao->close();
     }
-
-    if (trim($dados['nome']) === "") {
-        $erros['nome'] = 'nome é obrigatório';
-    }
-
-    if (trim($dados['telefone']) === "") {
-        $erros['telefone'] = 'telefone é obrigatório';
-    }
-
-
-    // if (!count($erros)) {
-    //     require_once "src/db/conexao.php";
-
-    //     $sql = "INSERT INTO veiculo (placa, motorista, telefone) VALUES (?, ?, ?)";
-
-    //     $conexao = novaConexao();
-    //     $stmt = $conexao->prepare($sql);
-
-    //     $params = [
-    //         $dados['placa'],
-    //         $dados['motorista'],
-    //         $dados['telefone'],
-    //     ];
-
-    //     $stmt->bind_param("ssi", ...$params);
-
-    //     if ($stmt->execute()) {
-    //         unset($dados); //Depois de inserir limpar os dados
-    //     }
-    // }
 }
 
 ?>
@@ -59,28 +60,28 @@ if (count($_POST) > 0) {
             </div>
         </div>
         <div class="card">
-            <form action="#" method="POST">
+            <form action="#" method="post">
                 <div class="form-row mt-3">
                     <div class="form-group col-md-4">
                         <label for="placa">Placa</label>
-                        <input type="text" id="placa" class="form-control" style="text-transform: uppercase" value="<?= $dados['placa'] ?>">
+                        <input type="text" name="placa" id="placa" class="form-control" 
+                        style="text-transform: uppercase" value="<?= $dados['placa'] ?>">
                     </div>
                     <div class="form-group col-md-4">
                         <label for="motorista">Motorista</label>
-                        <input type="text" id="motorista" class="form-control" style="text-transform: uppercase" value="<?= $dados['motorista'] ?>">
+                        <input type="text" name="motorista" id="motorista" class="form-control" 
+                        style="text-transform: uppercase" value="<?= $dados['motorista'] ?>">
                     </div>
                     <div class="form-group col-md-4">
                         <label for="telefone">Fone (Celular)</label>
-                        <input type="text" name="telefone" id="telefone" class="form-control" value="<?= $dados['telefone'] ?>">
+                        <input type="text" name="telefone" id="telefone" class="form-control" 
+                        value="<?= $dados['telefone'] ?>">
                     </div>
                 </div>
                 <div class="form-row btn-save  mt-3">
                     <button class="btn btn-lg btn-primary mt-3">
                         Salvar
                     </button>
-                    <?php
-                        print_r($_POST['telefone']);
-                    ?>
                 </div>
             </form>
         </div>
